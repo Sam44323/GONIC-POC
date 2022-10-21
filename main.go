b06ffe7da23e6b1f1e54d51450fee74b124a4f1a
Suranjan <suranjanbiswas2002@gmail.com>
2022-10-22 01:56:11 +0530

    remove: removed unused logs

--- main.go
+++ main.go
@@ -1,27 +1,11 @@
 package main
 
 import (
-  "github.com/gin-gonic/gin"
-  "github.com/Sam44323/gin-POC/service"
-  "github.com/Sam44323/gin-POC/controllers"
-  );
+	"github.com/gin-gonic/gin"
+)
 
-var(
-  videoService service.VideoService = service.New()
-  videoController controllers.VideoController = controllers.New(videoService)
-  )
+func main() {
+	server := gin.Default()
 
-func main(){
-  server := gin.Default();
-
-  server.GET("/videos", func(ctx *gin.Context){
-    ctx.JSON(200, videoController.FindAll())
-  });
-
-  server.POST("/videos", func(ctx *gin.Context){
-    ctx.JSON(200, videoController.Save(ctx))
-  })
-
-
-  server.Run(":8080");
+	server.Run(":8080")
 }
--- models/video.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type Video struct{
-  Title string `json:"title"` // json serlialization tag
-  Description string `json:"description"`
-  URL string `json:"url"`
-}
