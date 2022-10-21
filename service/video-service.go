09a92174cadfba56cf5756af9e92c60bcd457743
Suranjan <suranjanbiswas2002@gmail.com>
2022-10-22 01:14:31 +0530

    aadd: added interface structure for videoservice

--- service/video-service.go
+++ service/video-service.go
@@ -4,4 +4,24 @@ import "github.com/Sam44323/gin-POC/models";
 
 type VideoService interface {
   Save(video models.Video) models.Video
+  FindAll() []models.Video
+};
+
+type videoService struct{
+  videos []models.Video
+};
+
+func New() VideoService{
+  return &videoService{};
+}
+
+// implementing functions for the VideoService interface
+
+func (service *videoService) Save(video models.Video) models.Video{
+  service.videos = append(service.videos, video);
+  return video;
+}
+
+func (service *videoService) FindAll() []models.Video{
+  return service.videos;
 }
