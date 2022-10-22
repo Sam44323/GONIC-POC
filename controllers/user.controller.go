c7836ffab79e5c1114b6cadba936ed3a3ee398db
Suranjan <suranjanbiswas2002@gmail.com>
2022-10-22 16:17:15 +0530

    add: added struct initializer for controllers

--- controllers/user.controller.go
+++ controllers/user.controller.go
@@ -1,3 +1,13 @@
 package controllers
 
+import (
+  "github.com/Sam44323/gin-POC/services"
+  );
 
+type UserController struct{
+  UserService services.UserService
+}
+
+func New(userserive services.UserService) UserController {
+  return UserController{UserService: userserive}
+}
