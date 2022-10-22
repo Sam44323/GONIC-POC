97775f7db4e2dbbfc872e3f8195038095ee7aea3
Suranjan <suranjanbiswas2002@gmail.com>
2022-10-22 16:22:02 +0530

    add: added context-initialzier for gin

--- controllers/user.controller.go
+++ controllers/user.controller.go
@@ -1,8 +1,9 @@
 package controllers
 
 import (
-  "github.com/Sam44323/gin-POC/services"
-  );
+	"github.com/Sam44323/gin-POC/services"
+	"github.com/gin-gonic/gin"
+);
 
 type UserController struct{
   UserService services.UserService
@@ -10,4 +11,20 @@ type UserController struct{
 
 func New(userserive services.UserService) UserController {
   return UserController{UserService: userserive}
+}
+
+
+func (u *UserController) CreateUser(ctx *gin.Context){
+}
+
+func (u *UserController) GetUser(*gin.Context){
+}
+
+func (u *UserController) GetAll(*gin.Context) {
+}
+
+func (u *UserController) UpdateUser(*gin.Context) {
+}
+
+func (u *UserController) DeleteUser(*gin.Context) {
 }
