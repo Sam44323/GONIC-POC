ec17c3529d8b43bbf4c2c92b78ffed20984465f8
Suranjan <suranjanbiswas2002@gmail.com>
2022-10-22 15:16:25 +0530

    add: added interface for the user-service

---
+++ services/user.service.go
@@ -0,0 +1,12 @@
+package services
+
+import (
+  "github.com/Sam44323/gin-POC/models"
+  );
+
+type UserService interface{
+  CreateUser(*models.User) error
+  GetUser(*string) (*models.User, error) // either the user or any error will be returned
+  GetAll() ([]*models.User, error)
+  UpdateUser(*models.User) error
+}
