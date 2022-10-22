48b0639e67f04ea6cf3aa841295d705edab5bf45
Suranjan <suranjanbiswas2002@gmail.com>
2022-10-22 15:45:48 +0530

    add: added interface support for gomod

--- services/user.service.implement.go
+++ services/user.service.implement.go
@@ -1,1 +1,32 @@
 package services
+
+import (
+	"github.com/Sam44323/gin-POC/models"
+	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/net/context"
+);
+
+type UserServiceImpl struct{
+  usercollection *mongo.Collection
+  ctx context.Context
+}
+
+func (u *UserServiceImpl) CreateUser(user *models.User) error{
+  return nil
+}
+
+func (u *UserServiceImpl) GetUser(id *string) (*models.User, error){
+  return nil, nil
+}
+
+func (u *UserServiceImpl) GetAll() ([]*models.User, error){
+  return nil, nil
+}
+
+func (u *UserServiceImpl) UpdateUser(user *models.User) error{
+  return nil
+}
+
+func (u *UserServiceImpl) DeleteUser(id *string) error{
+  return nil
+}
