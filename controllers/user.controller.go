f96918b1ddcc06014ba5cff1ce2efe9614b361a3
Suranjan <suranjanbiswas2002@gmail.com>
2022-10-22 17:25:46 +0530

    add: added route-registrar for user

--- controllers/user.controller.go
+++ controllers/user.controller.go
@@ -33,3 +33,12 @@ func (u *UserController) UpdateUser(ctx *gin.Context) {
 func (u *UserController) DeleteUser(ctx *gin.Context) {
   ctx.JSON(200, nil);
 }
+
+func (u * UserController) RegisterUserRoutes(rg *gin.RouterGroup){
+  userRoute := rg.Group("/user"); // creating a base-path for the user-route
+  userRoute.POST("/create", u.CreateUser);
+  userRoute.GET("/get/:name", u.GetUser);
+  userRoute.GET("/getAll", u.GetAll);
+  userRoute.PUT("/update/:name", u.UpdateUser);
+  userRoute.DELETE("/delete/:name", u.DeleteUser);
+}
