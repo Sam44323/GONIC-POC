d28421961ac4eac908ee550406445342b821e6bb
Suranjan <suranjanbiswas2002@gmail.com>
2022-10-22 15:17:09 +0530

    update: updated the interface for user-controller

--- services/user.service.go
+++ services/user.service.go
@@ -9,4 +9,5 @@ type UserService interface{
   GetUser(*string) (*models.User, error) // either the user or any error will be returned
   GetAll() ([]*models.User, error)
   UpdateUser(*models.User) error
+  DeleteUser(*string) error
 }
