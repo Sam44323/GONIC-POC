44f96249a88e879683b30d5aa02107687f39e47a
Suranjan <suranjanbiswas2002@gmail.com>
2022-10-23 22:25:52 +0530

    add: added code for user-finder

--- controllers/user.controller.go
+++ controllers/user.controller.go
@@ -4,54 +4,62 @@ import (
 	"github.com/Sam44323/gin-POC/models"
 	"github.com/Sam44323/gin-POC/services"
 	"github.com/gin-gonic/gin"
-);
+)
 
-type UserController struct{
-  UserService services.UserService
+type UserController struct {
+	UserService services.UserService
 }
 
 func New(userserive services.UserService) UserController {
-  return UserController{UserService: userserive}
+	return UserController{UserService: userserive}
 }
 
+func (u *UserController) CreateUser(ctx *gin.Context) {
+	var user models.User
+	err := ctx.ShouldBindJSON(&user) // binding the user object to the data from the model
+	if err != nil {
+		ctx.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
 
-func (u *UserController) CreateUser(ctx *gin.Context){
-  var user models.User;
-  err := ctx.ShouldBindJSON(&user); // binding the user object to the data from the model
-  if err != nil {
-    ctx.JSON(400, gin.H{"error": err.Error()});
-    return;
-  }
-
-  e := u.UserService.CreateUser(&user);
-  if e != nil {
-    ctx.JSON(400, gin.H{"error": e.Error()});
-    return;
-  }
-  ctx.JSON(200, gin.H{"message": "User created successfully"});
+	e := u.UserService.CreateUser(&user)
+	if e != nil {
+		ctx.JSON(400, gin.H{"error": e.Error()})
+		return
+	}
+	ctx.JSON(200, gin.H{"message": "User created successfully"})
 }
 
-func (u *UserController) GetUser(ctx *gin.Context){
-  ctx.JSON(200, nil);
+func (u *UserController) GetUser(ctx *gin.Context) {
+	name := ctx.Param("name")
+	user, err := u.UserService.GetUser(&name)
+	if err != nil {
+		ctx.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	ctx.JSON(200, gin.H{
+		"message": "User found",
+		"user":    user,
+	})
 }
 
 func (u *UserController) GetAll(ctx *gin.Context) {
-  ctx.JSON(200, nil);
+	ctx.JSON(200, nil)
 }
 
 func (u *UserController) UpdateUser(ctx *gin.Context) {
-  ctx.JSON(200, nil);
+	ctx.JSON(200, nil)
 }
 
 func (u *UserController) DeleteUser(ctx *gin.Context) {
-  ctx.JSON(200, nil);
+	ctx.JSON(200, nil)
 }
 
-func (u * UserController) RegisterUserRoutes(rg *gin.RouterGroup){
-  userRoute := rg.Group("/user"); // creating a base-path for the user-route
-  userRoute.POST("/create", u.CreateUser);
-  userRoute.GET("/get/:name", u.GetUser);
-  userRoute.GET("/getAll", u.GetAll);
-  userRoute.PATCH("/update/:name", u.UpdateUser);
-  userRoute.DELETE("/delete/:name", u.DeleteUser);
+func (u *UserController) RegisterUserRoutes(rg *gin.RouterGroup) {
+	userRoute := rg.Group("/user") // creating a base-path for the user-route
+	userRoute.POST("/create", u.CreateUser)
+	userRoute.GET("/get/:name", u.GetUser)
+	userRoute.GET("/getAll", u.GetAll)
+	userRoute.PATCH("/update/:name", u.UpdateUser)
+	userRoute.DELETE("/delete/:name", u.DeleteUser)
 }
--- services/user.service.implement.go
+++ services/user.service.implement.go
@@ -2,38 +2,42 @@ package services
 
 import (
 	"github.com/Sam44323/gin-POC/models"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"golang.org/x/net/context"
-);
+)
 
-type UserServiceImpl struct{
-  usercollection *mongo.Collection
-  ctx context.Context
+type UserServiceImpl struct {
+	usercollection *mongo.Collection
+	ctx            context.Context
 }
 
 func NewUserServiceImpl(usercollection *mongo.Collection, ctx context.Context) *UserServiceImpl {
-  return &UserServiceImpl{usercollection, ctx}  // creating a new struct
+	return &UserServiceImpl{usercollection, ctx} // creating a new struct
 }
 
 // implementation methods for the interface (automatically inferred)
 
-func (u *UserServiceImpl) CreateUser(user *models.User) error{
-  _, err := u.usercollection.InsertOne(u.ctx, user);
-  return err;
-};
-
-func (u *UserServiceImpl) GetUser(name *string) (*models.User, error){
-  return nil, nil; 
+func (u *UserServiceImpl) CreateUser(user *models.User) error {
+	_, err := u.usercollection.InsertOne(u.ctx, user)
+	return err
 }
 
-func (u *UserServiceImpl) GetAll() ([]*models.User, error){
-  return nil, nil
+func (u *UserServiceImpl) GetUser(name *string) (*models.User, error) {
+	var user *models.User
+	querym := bson.D{bson.E{Key: "name", Value: name}}
+	err := u.usercollection.FindOne(u.ctx, querym).Decode(&user)
+	return user, err
 }
 
-func (u *UserServiceImpl) UpdateUser(user *models.User) error{
-  return nil
+func (u *UserServiceImpl) GetAll() ([]*models.User, error) {
+	return nil, nil
 }
 
-func (u *UserServiceImpl) DeleteUser(id *string) error{
-  return nil
+func (u *UserServiceImpl) UpdateUser(user *models.User) error {
+	return nil
+}
+
+func (u *UserServiceImpl) DeleteUser(id *string) error {
+	return nil
 }
