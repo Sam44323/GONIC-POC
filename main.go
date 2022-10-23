7a73cdf38beae2134d2b89a77cb0167e7b6d5e70
Suranjan <suranjanbiswas2002@gmail.com>
2022-10-24 00:51:30 +0530

    add: added variable declaration for main

--- main.go
+++ main.go
@@ -3,7 +3,22 @@ package main
 //go:generate go run main.go
 
 import (
+	"context"
+
+	"github.com/Sam44323/gin-POC/controllers"
+	"github.com/Sam44323/gin-POC/services"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var (
+	server         *gin.Engine
+	userservice    services.UserService
+	usercontroller controllers.UserController
+	ctx            context.Context
+	usercollection *mongo.Collection
+	mongoclient    *mongo.Client
+	err            error
 )
 
 func main() {
