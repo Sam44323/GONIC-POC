66ff1942508e581ca55d9495233afce3003dd38e
Suranjan <suranjanbiswas2002@gmail.com>
2022-10-24 01:01:16 +0530

    add: added initializer for mongo

--- main.go
+++ main.go
@@ -4,11 +4,14 @@ package main
 
 import (
 	"context"
+	"log"
 
 	"github.com/Sam44323/gin-POC/controllers"
 	"github.com/Sam44323/gin-POC/services"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 var (
@@ -21,8 +24,20 @@ var (
 	err            error
 )
 
+func init() {
+	ctx = context.TODO() // a simple context creation with no cancellation or timeouts
+	mongoconnection := options.Client().ApplyURI("mongodb://localhost:27017")
+	mongoclient, err = mongo.Connect(ctx, mongoconnection)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = mongoclient.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err) // ping the server to verify that the connection is established to the primary-database or not
+	}
+}
+
 func main() {
 	server := gin.Default()
-
 	server.Run(":8080")
 }
